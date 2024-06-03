b5b42fef7bb9d6c5a235ed168247f3b10f6cbfb2
Axx8 <i@iis.cm>
2024-06-03 19:03:37 +0800

    Create 恶搞.ino

---
+++ 恶搞.ino
@@ -0,0 +1,27 @@
+Keyboard.println("notepad.exe");    //打开记事本
+  delay(500);
+  Keyboard.println("                                   $$$$ ");
+  delay(500);
+  Keyboard.println("                               $$         $$");
+  Keyboard.println("                               $$         $$");
+  Keyboard.println("                               $$         $$");
+  Keyboard.println("                               $$         $$");
+  Keyboard.println("                               $$         $$");
+  Keyboard.println("                               $$         $$");
+  Keyboard.println("                        $$$$$$         $$$$$$");
+  Keyboard.println("   $$$$$$     $$         $$         $$        $$$$");
+  Keyboard.println("   $$         $$$$         $$         $$        $$    $$");
+  Keyboard.println("   $$             $$         $$         $$        $$        $$");
+  Keyboard.println("        $$        $$                                 $$         $$");
+  Keyboard.println("          $$$    $$                                              $$");
+  Keyboard.println("            $$                                                      $$");
+  Keyboard.println("              $$$                                                  $$");
+  Keyboard.println("                $$                                                  $$");
+  Keyboard.println("                  $$$                                              $$");
+  Keyboard.println("                    $$                                          $$$");
+  Keyboard.println("                      $$$                                      $$");
+  Keyboard.println("                        $$                                      $$");
+  Keyboard.println("                          $$$                              $$$");
+  Keyboard.println("                            $$                              $$");
+  Keyboard.println("                            $$$$$$$$$$$$$$$$$$$$");
+  delay(500);
